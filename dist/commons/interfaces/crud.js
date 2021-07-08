9ea1e859546e8cc8b32070543c4b8adabe295f13
Obioha Uche <radioactive.uche11@gmail.com>
2021-07-08 20:35:52 +0100

    .gitignore fix

--- dist/commons/interfaces/crud.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3J1ZC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL2NvbW1vbnMvaW50ZXJmYWNlcy9jcnVkLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
